7ccb3409ea8692e1e09c7f282f9feba492a5d767
Karol <michkarol.2000@wp.pl>
2021-12-21 22:57:02 +0100

    fix knight animation

--- src/characters/Knight.ts
+++ src/characters/Knight.ts
@@ -41,6 +41,7 @@ export default class Knight extends Phaser.Physics.Arcade.Sprite
     }
 
     preUpdate(t: number, dt: number){
+        super.preUpdate(t, dt)
         switch (this.healthState)
         {
             case HealthState.IDLE:
